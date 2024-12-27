4dd8cbd4acd423e23714457c379ba73f9a0d2a7e
Ankit Warbhe <ankitwarbheofficial@gmail.com>
2024-12-28 05:18:57 +0530

    .

--- src/server.js
+++ src/server.js
@@ -8,6 +8,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const clients = new Map([
+  ['7b2e685bf08b5cb99c36518619c024c1ed1333e8f4eaae4ef5c5d4e29ca0b70e7dcf21e0fa08a0b29b076e9d5cd420cce6eba1f4f433e906cdc6c743907ef696', {
+    clientId: '7b2e685bf08b5cb99c36518619c024c1ed1333e8f4eaae4ef5c5d4e29ca0b70e7dcf21e0fa08a0b29b076e9d5cd420cce6eba1f4f433e906cdc6c743907ef696',
+    clientSecret: '4f70c91e5390f5b0fd9bbc28e5b9f9ad8f9341ac5c42ad33f295387333eb10106555fa52957427c01038bcfc05264f8b895422fd8d01ca1448b3c16c5d9bd6aa',
+    redirectUris: [
+      'https://extensionesign-server.vercel.app/callback',
+      'http://localhost:3000/callback',
+      'https://oauth.pstmn.io/v1/callback'
+    ]
+  }]
+]);
+
 // Authorization endpoint
 app.get('/oauth2/authorize', (req, res) => {
   const { client_id, redirect_uri, response_type, scope, state } = req.query;
